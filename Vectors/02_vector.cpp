ea93621efe81e964624a51a12c451c05528c370f
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-16 00:31:58 +0530

    Vector Memory Allocation

--- Vectors/02_vector.cpp
+++ Vectors/02_vector.cpp
@@ -2,18 +2,25 @@
 vector<int> vec1;
 vector<int> vec2(5,-1)
 //Implementation in Memory
+size-> number of elements
+capacity-> max.  number of elements it can hold when we try to add
+more capacity than another location double size is used as vector
 */
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
     vector<int> vec1;
-    cout<<vec1.size()<<endl;
-    vector<int> vec2={1,2,3,4,5};
-    cout<<vec2.size()<<endl;
-    vector<int> vec3={10,0};
-    cout<<vec3.size()<<endl;
+    cout << vec1.size() << endl;
+    vector<int> vec3 = {10, 0};
+    cout << vec3.size() << endl;
+    vector<int> vec2 = {1, 2, 3, 4, 5};
+    cout <<"Size: "<< vec2.size() << endl;
+    cout << "Capacity: "<< vec2.capacity() << endl;
+    vec2.push_back(4);
+     cout <<"Size: "<< vec2.size() << endl;
+    cout << "Capacity: "<< vec2.capacity() << endl;// capacity doubled
     return 0;
 }
