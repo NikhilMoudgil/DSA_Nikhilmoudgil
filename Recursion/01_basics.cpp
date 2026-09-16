0481a79fb9f0974c5b36c494ec56b7f84764bed6
NikhilMoudgil <nikhilmoudgil799@gmail.com>
2026-09-16 20:45:05 +0530

    recursion basic

---
+++ Recursion/01_basics.cpp
@@ -0,0 +1,14 @@
+//when a function calls itself is called recursive function and process is called recursion
+/* main func-> rec func -> rec func ->rec func ->rec func -> ...
+ end point (base case ) and then return,
+f(n)= n* f(n-1) -> recurrence relation 
+
+*/
+#include<iostream>
+using namespace std;
+ recFunc()
+int main()
+{
+    
+    return 0;
+}
